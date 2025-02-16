11ab65b914bc0e0fac7137614ab5865d7e115dd3
John Kehike <kehikejohnbull@yahoo.com>
2025-02-16 17:20:21 +0000

    youtube sunday update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let actionBox = document.getElementById('action-box');
+// let actionBox = document.getElementById('action-box');
 let videoMenus = document.querySelectorAll('.fa-ellipsis-vertical');
 let openInputMobile = document.getElementById('open-input');
 let mobileInput = document.getElementById('nav-mini');
@@ -87,21 +87,11 @@ document.querySelectorAll('.vid').forEach((menu, index)=>{
   
   return `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
-  // progressBar.forEach((progressB,index)=>{
-  //   progressB.style.width = `${progressPercent}%`;
-  // })
-  // progressBar.style.width = `${progressPercent}%`;
+
 })
 
 
-// Seek Video on Click
-// progressContainer.addEventListener("click", (e) => {
-//   const width = progressContainer.clientWidth;
-//   const clickX = e.offsetX;
-//   const duration = vid.duration;
-  
-//   vid.currentTime = (clickX / width) * duration;
-// });
+
 progressContainer.forEach((progressC, index)=>{
   progressC.addEventListener('click', (e) =>{
     const width = progressC.clientWidth;
@@ -114,9 +104,7 @@ progressContainer.forEach((progressC, index)=>{
     
     videc.currentTime = (clickX/width) * duration;
     
-    // vid.forEach((vidy)=>{
-    //   const duration = vidy.duration;
-    // })
+
     
   })
 })
@@ -130,23 +118,7 @@ document.querySelector('.fa-volume-xmark').addEventListener('click',(event) => {
   
   
 })
-// document.querySelectorAll('.fa-volume-xmark').forEach((mute, index) =>{
-//   mute.addEventListener('click', (event) =>{
-//     let parent = event.target.closest('.vid');
-//     console.log(event);
-//     // let cont = parent.document.querySelector('.vid')
-    
-//     console.log(parent);
-    
-//    cont.muted = !cont.muted;
-//   }
 
-    
-//     // vid.muted = !vid.muted;
-//     // muteBtn.classList.toggle("fa-volume-mute", !vid.muted);
-//     // muteBtn.classList.toggle("fa-volume-up", vid.muted);
-//   )
-// })
 
 openInputMobile.addEventListener('click', openMobileSearch);
 closeInput.addEventListener('click', openMobileSearch);
@@ -163,13 +135,7 @@ function openMobileSearch(e){
   }
 
 }
-// document.getElementById('vide').addEventListener('click',(e)=>{
-//   console.log('mouse');
-//   console.log(e.target);
-  
-//   vid.play();
-  
-// })
+
 document.querySelectorAll('.vid').forEach((eachVid, index)=>{
   
   eachVid.addEventListener('click',()=>{
@@ -188,7 +154,7 @@ document.querySelectorAll('.fa-ellipsis-vertical').forEach((menu, index) => {
 document.addEventListener('click', closeActionBox);
 
 function openActionBox(e){
-    console.log(e.target.closest);
+    // console.log(e.target.closest);
     let parent = this.closest('.video-text');
     let child = parent.querySelector('.non-action-box, .action-box');
 
@@ -223,49 +189,26 @@ function openActionBox(e){
 
 
 function closeActionBox(event) {
-    let clickedInsideMenu = Array.from(videoMenus).some(menu => menu.contains(event.target));
-    if (!actionBox.contains(event.target) && !clickedInsideMenu) {
-        actionBox.classList.remove('action-box');
-        actionBox.classList.add('non-action-box');
-        // child.style.left = '';
-        // child.style.right = '';
+  console.log('mine');
+  
+  // !actionBox.contains(event.target) && !clickedInsideMenu
+  let clickedInsideMenu = Array.from(videoMenus).some(menu => menu.contains(event.target));
+  document.querySelectorAll('.valid').forEach((actionBoxx, index)=>{
+    if (!actionBoxx.contains(event.target) && !clickedInsideMenu) {
+      actionBoxx.classList.remove('action-box');
+      actionBoxx.classList.add('non-action-box');
     }
+  })
+ 
+    // if () {
+    //     actionBox.classList.remove('action-box');
+    //     actionBox.classList.add('non-action-box');
+   
+    // }
 }
 
 function refreshPage(){
   setTimeout(function() {
     location.reload();
-  }, 60000); // Refresh the page after 5 seconds
+  }, 60000); 
 }
-// refreshPage()
-
-function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    gapi.client.setApiKey("YAIzaSyDnljhbOU0vrE3g-hVpGLeaZE7LNo4Qgjo");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return gapi.client.youtube.videos.list({
-      "part": [
-        "snippet,contentDetails,statistics"
-      ],
-      "chart": "mostPopular",
-      "regionCode": "US"
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "725810913778-jpqlsiqab0iqgoc48ncroin0sh9ukp4m.apps.googleusercontent.com"});
-  });
