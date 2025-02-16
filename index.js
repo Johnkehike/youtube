9536065bacbe95730407dc7556452a20eb1502a1
John Kehike <kehikejohnbull@yahoo.com>
2025-02-16 12:06:56 +0000

    youtube sunday update

--- index.js
+++ index.js
@@ -4,6 +4,103 @@ let openInputMobile = document.getElementById('open-input');
 let mobileInput = document.getElementById('nav-mini');
 let otherInput = document.getElementById('nav');
 let closeInput = document.getElementById('close-mobile-search');
+let vid = document.querySelector('.vid');
+const progressBar = document.querySelectorAll(".progress-bar");
+const progressContainer = document.querySelectorAll(".progress-container");
+// console.log(vid);
+
+// Update Progress Bar
+// vid.addEventListener("timeupdate", () => {
+//   const progressPercent = (vid.currentTime / vid.duration) * 100;
+//   progressBar.style.width = `${progressPercent}%`;
+// });
+
+document.querySelectorAll('.vid').forEach((menu, index)=>{
+  menu.addEventListener('timeupdate', (e)=>{
+    const progressPercent = (menu.currentTime / menu.duration) * 100;
+    let newProgressBar = e.target.parentElement.querySelector('.progress-bar');
+    newProgressBar.style.width = `${progressPercent}%`;
+    // let timing = e.target.parentElement.querySelector('.video-container-additions').querySelector('.vid-two-group').querySelector('.video-time');
+    // console.log(timing);
+    let tu = e.target.parentElement.children[1].children[1].children[0];
+    
+    // console.log(e.target.parentElement.children[1].children[1].children[0]);
+    
+    // tu.textcontent = formatTime(menu.currentTime);
+    tu.innerHTML = formatTime(menu.currentTime); // Instead of textContent
+    tu.offsetHeight;
+    
+    
+
+  })
+  
+  function formatTime(seconds) {
+  let hrs = Math.floor(seconds / 3600);
+  let mins = Math.floor((seconds % 3600) / 60);
+  let secs = Math.floor(seconds % 60);
+  
+  return `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+  // progressBar.forEach((progressB,index)=>{
+  //   progressB.style.width = `${progressPercent}%`;
+  // })
+  // progressBar.style.width = `${progressPercent}%`;
+})
+
+
+// Seek Video on Click
+// progressContainer.addEventListener("click", (e) => {
+//   const width = progressContainer.clientWidth;
+//   const clickX = e.offsetX;
+//   const duration = vid.duration;
+  
+//   vid.currentTime = (clickX / width) * duration;
+// });
+progressContainer.forEach((progressC, index)=>{
+  progressC.addEventListener('click', (e) =>{
+    const width = progressC.clientWidth;
+    const clickX = e.offsetX;
+    let videc = e.target.parentElement.parentElement.querySelector('.vid');
+    console.log(videc);
+    
+    const duration = videc.duration;
+    
+    
+    videc.currentTime = (clickX/width) * duration;
+    
+    // vid.forEach((vidy)=>{
+    //   const duration = vidy.duration;
+    // })
+    
+  })
+})
+
+document.querySelector('.fa-volume-xmark').addEventListener('click',(event) => {
+  console.log(event.target.parentElement.parentElement.parentElement);
+  let parent = event.target.parentElement.parentElement.parentElement;
+  let child = parent.querySelector('.vid');
+  console.log(child);
+  child.muted = !child.muted;
+  
+  
+})
+// document.querySelectorAll('.fa-volume-xmark').forEach((mute, index) =>{
+//   mute.addEventListener('click', (event) =>{
+//     let parent = event.target.closest('.vid');
+//     console.log(event);
+//     // let cont = parent.document.querySelector('.vid')
+    
+//     console.log(parent);
+    
+//    cont.muted = !cont.muted;
+//   }
+
+    
+//     // vid.muted = !vid.muted;
+//     // muteBtn.classList.toggle("fa-volume-mute", !vid.muted);
+//     // muteBtn.classList.toggle("fa-volume-up", vid.muted);
+//   )
+// })
 
 openInputMobile.addEventListener('click', openMobileSearch);
 closeInput.addEventListener('click', openMobileSearch);
@@ -20,17 +117,28 @@ function openMobileSearch(e){
   }
 
 }
-
-// function closeMobilesearch(){
-
-// }
-
+// document.getElementById('vide').addEventListener('click',(e)=>{
+//   console.log('mouse');
+//   console.log(e.target);
+  
+//   vid.play();
+  
+// })
+document.querySelectorAll('.vid').forEach((eachVid, index)=>{
+  
+  eachVid.addEventListener('click',()=>{
+    eachVid.play();
+  })
+});
+document.querySelectorAll('.vid').forEach((eachVid, index)=>{
+  eachVid.addEventListener('mouseleave',()=>{
+    eachVid.pause();
+    eachVid.currentTime = 0;
+  })
+});
 document.querySelectorAll('.fa-ellipsis-vertical').forEach((menu, index) => {
-    menu.addEventListener('click', openActionBox)
+    menu.addEventListener('click', openActionBox);
 })
-
-
-
 document.addEventListener('click', closeActionBox);
 
 function openActionBox(e){
@@ -41,15 +149,8 @@ function openActionBox(e){
         if (child.classList.contains('non-action-box')) {
             child.classList.remove('non-action-box');
             child.classList.add('action-box');
-            // child.classList.add('action-box-end');
-            // child.style.right = `0rem`;
-        // }else if (child.classList.contains('non-action-box') && rect.right < viewportWidth) {
-        //     child.classList.remove('non-action-box');
-        //     child.classList.remove('action-box-end');
-        //     child.classList.add('action-box');
         }else{
             child.classList.remove('action-box');
-            // child.classList.remove('action-box-end');
             child.classList.add('non-action-box');
         }
         
